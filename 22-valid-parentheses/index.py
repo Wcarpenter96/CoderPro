a3d6206ce406f2b39a2d0a759346117c9bd26d47
Weston Carpenter <wc.cc96@gmail.com>
2020-01-01 13:23:21 -0800

    session 22 added

---
+++ 22-valid-parentheses/index.py
@@ -0,0 +1,41 @@
+# use a stack (first in, last out)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            
+            if c == '(':
+                stack.append('(')
+            if c == ')':
+                if len(stack) == 0:
+                    return False
+                if stack[-1] != '(':
+                    return False
+                else:
+                    stack.pop()
+            
+            if c == '{':
+                stack.append('{')
+            if c == '}':
+                if len(stack) == 0:
+                    return False
+                if stack[-1] != '{':
+                    return False
+                else:
+                    stack.pop()
+            
+            if c == '[':
+                stack.append('[')
+            if c == ']':
+                if len(stack) == 0:
+                    return False
+                if stack[-1] != '[':
+                    return False
+                else:
+                    stack.pop()
+
+        if len(stack) > 0:
+            return False
+        else:
+            return True
