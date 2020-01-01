c7637020aca2aaaf874792ba579add0015bc636c
Weston Carpenter <wc.cc96@gmail.com>
2020-01-01 14:12:49 -0800

    session 24 added

---
+++ 24-3-sum/index.py
@@ -0,0 +1,38 @@
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue 
+        # two sum
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1 
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            # end two sum
+        return res
+    def threeSumSlow(self,nums):
+        res = set()
+        nums.sort()
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        res.add((nums[i],nums[j],nums[k]))
+        return list(res)
+
+
+
+print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
